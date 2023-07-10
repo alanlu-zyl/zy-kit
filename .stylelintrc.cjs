7e88658302be4b462c059a9cdbd49f2a2b9daf17
Alan Lu <alanlu.zyl@gmail.com>
2023-07-10 11:18:55 +0800

    chore(lint): :hammer: max_lint_length = 200

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -10,12 +10,7 @@ module.exports = {
   rules: {
     'no-empty-source': null,
     // 允許 global、export、v-deep 等偽類
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        ignorePseudoClasses: ['global', 'export', 'v-deep', 'deep'],
-      },
-    ],
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'export', 'v-deep', 'deep'] }],
     /** selector class pattern must match [BEM CSS](https://en.bem.info/methodology/css) - [Regex](https://regexr.com/3apms) */
     'selector-class-pattern': [
       '^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',

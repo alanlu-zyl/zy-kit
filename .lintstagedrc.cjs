686fc59d8560b8a43c78296d02cef16308c1ff52
Zyl <mk816017@gmail.com>
2023-07-17 04:11:17 +0800

    chore: markdownlint 不使用 prettier

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  'package.json': ['prettier --write'],
+  'package.json': ['eslint --fix', 'prettier --write'],
   '{!(package)*.json,*.code-snippets,.!(browserslist|npm)*rc}': ['prettier --write--parser json'],
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.vue': ['eslint --fix', 'prettier --write', 'stylelint --fix'],
+  '*.vue': ['eslint --fix', 'stylelint --fix', 'prettier --write'],
   '*.{css,scss,postcss,less,html}': ['stylelint --fix', 'prettier --write'],
   '*.{md,mdx}': ['markdownlint-cli2-fix'],
 }
